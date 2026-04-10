478c5877563e72613965bf591b0d71319f26a882
ale_andrew <grandrew001@gmail.com>
2026-04-10 12:23:36 +0300

    Небольшой рефакторинг после ревью наставника

--- a_expense.py
+++ a_expense.py
@@ -1,11 +1,6 @@
-
-
 class Expense:
     def __init__(self, category, amount, description):
         self.category = category
         self.amount = amount
         self.description = description
-
-    def show(self):
-        """Показать один расход"""
-        print(f"{self.category} | {self.amount} руб. | {self.description}")
+        
